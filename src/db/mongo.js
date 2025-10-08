fe1f9760bd0679dd0495ea423c5e8b8cc678b861
kushal <kushal@itobuz.com>
2025-10-08 15:48:14 +0530

    feat: tasks schema

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -3,9 +3,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const uri_pass = process.env.MONGO_PASS
-
-const uri = `mongodb+srv://kushal_db_user:${uri_pass}@cluster0.tb6qusq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const uri = process.env.MONGO_PASS
 
 export default async function connectDB() {
   try {
---
+++ src/model/taskModel.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    isCompleted: {
+      type: Boolean,
+      default: false,
+    },
+    isImpotant: {
+      type: Boolean,
+      default: false,
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Task = mongoose.model('Task', taskSchema)
+
+export default Task
