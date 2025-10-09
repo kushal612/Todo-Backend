12a216fcb30f3330e876b3613f280b4d637556ba
kushal <kushal@itobuz.com>
2025-10-09 10:14:49 +0530

    feat: todo validation schema

---
+++ src/validation/schema/todoValidationSchema.js
@@ -0,0 +1,27 @@
+import * as yup from 'yup'
+//import randomUUID from 'crypto'
+
+export const postSchema = yup.object({
+  //id: yup.string().default(() => randomUUID()),
+  title: yup
+    .string()
+    .trim()
+    .min(3, 'Title must be at least 3 characters long')
+    .required('Title is required'),
+  isCompleted: yup.boolean().default(false),
+
+  body: yup.string().required(),
+  tags: yup.array().optional(),
+  isImpotant: yup.boolean().default(false),
+})
+
+export const updateSchema = yup.object({
+  title: yup
+    .string()
+    .trim()
+    .min(3, 'Title must be at least 3 characters long')
+    .optional(),
+  isCompleted: yup.boolean().optional(),
+  tags: yup.array().optional(),
+  isImpotant: yup.boolean().optional(),
+})
